66e51b521438476f3ab4ca8f214fa3ccf7b87ca3
aleph-oh <jhilton@mit.edu>
2021-07-05 17:08:28 -0400

    Make database fields not nullable

--- database/models.py
+++ database/models.py
@@ -10,13 +10,13 @@ class Link(Base):
     __tablename__ = "link"
 
     id = Column(Integer, primary_key=True)
-    src = Column(Integer, ForeignKey("article.id"))
-    dst = Column(Integer, ForeignKey("article.id"))
+    src = Column(Integer, ForeignKey("article.id"), nullable=False)
+    dst = Column(Integer, ForeignKey("article.id"), nullable=False)
 
 
 class Article(Base):
     __tablename__ = "article"
 
     id = Column(Integer, primary_key=True)
-    title = Column(Text)
-    links = relationship("Link", backref="source", foreign_keys=[Link.src])
+    title = Column(Text, nullable=False)
+    links = relationship("Link", backref="origin", foreign_keys=[Link.src])
