62055792ce4f9b5343bcca04c03a022d96d7f5cf
aleph-oh <jhilton@mit.edu>
2021-07-06 00:48:59 -0400

    Refactor article retrieval script into callable fn

--- web/build_database.py
+++ web/build_database.py
@@ -5,14 +5,28 @@ from sqlalchemy.orm import Session as SessionTy
 import pywikibot  # type: ignore
 from database import Session, Article, Link
 
-site = pywikibot.Site("en")
-db: SessionTy = Session()
-for _page in site.allpages():
-    page = cast(pywikibot.Page, _page)
-    linked_pages = cast(Iterable[pywikibot.Page], page.linkedPages())
-    db_links = [
-        Link(src=page.pageid, dst=linked_page.pageid) for linked_page in linked_pages
-    ]
-    db_article = Article(id=page.pageid, title=page.title(), links=db_links)  # type: ignore
-    db.add(db_article)
-    db.commit()
+__all__ = [
+    "add_articles_to_db"
+]
+
+
+def add_articles_to_db(session: SessionTy = None) -> None:
+    if session is None:
+        session = Session()
+    site = pywikibot.Site("en")
+    for _page in site.allpages():
+        page = cast(pywikibot.Page, _page)
+        linked_pages = cast(Iterable[pywikibot.Page], page.linkedPages())
+        db_links = [
+            Link(src=page.pageid, dst=linked_page.pageid)
+            for linked_page in linked_pages
+        ]
+        db_article = Article(
+            id=page.pageid, title=page.title(), links=db_links
+        )  # type: ignore
+        session.add(db_article)
+        session.commit()
+
+
+if __name__ == "__main__":
+    add_articles_to_db()
