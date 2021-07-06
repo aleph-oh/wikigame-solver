ad57f5a1f14be2b12b8f89dbc41ec807c9d2f321
aleph-oh <jhilton@mit.edu>
2021-07-06 00:45:36 -0400

    Make database builder script typecheck

--- web/build_database.py
+++ web/build_database.py
@@ -1,18 +1,18 @@
-from typing import cast
+from typing import Iterable, cast
 
 from sqlalchemy.orm import Session as SessionTy
 
-import pywikibot
+import pywikibot  # type: ignore
 from database import Session, Article, Link
 
 site = pywikibot.Site("en")
 db: SessionTy = Session()
 for _page in site.allpages():
     page = cast(pywikibot.Page, _page)
+    linked_pages = cast(Iterable[pywikibot.Page], page.linkedPages())
     db_links = [
-        Link(src=page.pageid, dst=linked_page.pageid)
-        for linked_page in page.linkedPages()
+        Link(src=page.pageid, dst=linked_page.pageid) for linked_page in linked_pages
     ]
-    db_article = Article(id=page.pageid, title=page.title(), links=db_links)
+    db_article = Article(id=page.pageid, title=page.title(), links=db_links)  # type: ignore
     db.add(db_article)
     db.commit()
