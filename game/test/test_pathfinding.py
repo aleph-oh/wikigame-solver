5dc4730cc2243817b09c9ee11cc601bd2dd8bab9
aleph-oh <jhilton@mit.edu>
2021-07-07 11:07:41 -0400

    Export parent pointer follow, change pathing test
    - Revised pathing test to only generate simple graphs:
      this is a simple thing to filter for in the real inputs

--- game/test/test_pathfinding.py
+++ game/test/test_pathfinding.py
@@ -51,6 +51,8 @@ def test_follow_parent_pointers_recurrence(parents_dst):
         parent_path = follow_parent_pointers(ancestor, parents)
         if parent_path is not None:
             assert path == parent_path + [dst_id]
+        else:
+            assert path is None
     if path is not None:
         assert len(set(path)) == len(path)
 
@@ -69,7 +71,7 @@ def adjacency_lists(
     keys_list = list(keys)
     return draw(
         st.fixed_dictionaries(
-            {k: st.sets(st.sampled_from(keys_list)) for k in keys}
+            {k: st.sets((st.sampled_from(keys_list).filter(k.__ne__))) for k in keys}
         ).filter(
             lambda d: min_edges <= sum(len(edges) for edges in d.values()) <= max_edges
         )
@@ -110,7 +112,7 @@ def add_graph_to_db(session: Session, graph: Mapping[int, set[int]]) -> None:
 @given(inputs=two_nodes_and_graph())
 def test_single_multi_target_equivalent(
     inputs: tuple[Mapping[int, set[int]], int, int]
-):
+) -> None:
     graph, src, dst = inputs
     with session_scope() as session:
         add_graph_to_db(session, graph)
@@ -125,15 +127,14 @@ def test_single_multi_target_equivalent(
 
 
 @given(inputs=two_nodes_and_graph(), data=st.data())
-def test_optimal_substructure(
+def test_single_target_optimal_substructure(
     inputs: tuple[Mapping[int, set[int]], int, int], data: DataObject
-):
+) -> None:
     graph, src, dst = inputs
     with session_scope() as session:
         add_graph_to_db(session, graph)
         path = single_target_bfs(session, str(src), str(dst))
         if path is None:
-            assert set() in graph.values()
             return
         # shortest paths have optimal substructure:
         # if the path is P[0..k], P[i..j] is a shortest path from i to j
